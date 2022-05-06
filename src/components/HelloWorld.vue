c2939a4c7e3956f800ed9ec510a39a95d0dbeb85
Paul J <jorpaulvincent@gmail.com>
2022-05-06 19:35:11 +0800

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div id="calculator" style="width: 600px; height: 400px"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style scoped>
+#calculator {
+  margin: auto;
+  border: 1px solid black;
+}
+</style>
